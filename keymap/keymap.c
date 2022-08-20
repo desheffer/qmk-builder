25a60da2a2f3651c824228dbb4a4a0d775fc9b68
Doug Sheffer <desheffer@gmail.com>
2022-08-20 12:28:18 -0400

    Simplify Qwerty layer

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -92,21 +92,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * │ Tab    │ q      │ w      │ e      │ r      │ t      │ y      │ u      │ i      │ o      │ p      │ Bkspc  │
  * │        │        │        │        │        │        │        │        │        │        │        │        │
  * ├────────╔════════╦════════╦════════╦════════╗────────┼────────╔════════╦════════╦════════╦════════╗────────┤
- * │ Esc    ║ a      ║ s      ║ d      ║ f      ║ g      │ h      ║ j      ║ k      ║ l      ║ '      ║ Enter  │
- * │[Ctrl]  ║        ║        ║        ║        ║        │        ║        ║        ║        ║        ║[Ctrl]  │
+ * │ Esc    ║ a      ║ s      ║ d      ║ f      ║ g      │ h      ║ j      ║ k      ║ l      ║ ;      ║ Enter  │
+ * │        ║        ║        ║        ║        ║        │        ║        ║        ║        ║        ║        │
  * ├────────╚════════╩════════╩════════╩════════╝────────┼────────╚════════╩════════╩════════╩════════╝────────┤
  * │        │ z      │ x      │ c      │ v      │ b      │ n      │ m      │ ,      │ .      │ /      │        │
- * │[Numpad]│        │        │        │        │        │        │        │        │        │        │[Shift] │
+ * │[Shift] │        │        │        │        │        │        │        │        │        │        │[Shift] │
  * ├────────┼────────┼────────┼────────╔════════╦═════════════════╦════════╗────────┼────────┼────────┼────────┤
  * │        │        │        │ AltGr  ║        ║ Space           ║        ║ ←      │ ↓      │ ↑      │ →      │
- * │[Ctrl]  │[Fn]    │[GUI]   │[Alt]   ║[Symbol]║[Navigation]     ║[Shift] ║        │        │        │        │
+ * │[Ctrl]  │[Fn]    │[GUI]   │[Alt]   ║[Symbol]║                 ║[Shift] ║        │        │        │        │
  * └────────┴────────┴────────┴────────╚════════╩═════════════════╩════════╝────────┴────────┴────────┴────────┘
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, CTL_ENT,
-    NUMPAD,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, FN,      KC_LGUI, ALT_GR,  SYMBOL,  NAV_SPC, NAV_SPC, SHIFT,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LCTL, FN,      KC_LGUI, ALT_GR,  SYMBOL,  KC_SPC,  KC_SPC,  SHIFT,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Navigation
