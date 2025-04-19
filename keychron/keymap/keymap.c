81f37c3cdf7bdc7e3fe13f8a92601bc0ef3de2bf
Doug Sheffer <desheffer@gmail.com>
2025-04-18 23:27:23 -0400

    Set TAPPING_TERM

--- keychron/keymap/keymap.c
+++ keychron/keymap/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGDN,
     CTL_ESC, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_SCLN,          CTL_ENT,          KC_HOME,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT, KC_UP,
-    KC_LCMD, KC_LALT, SYMBOL,                    NAV_SPC,                            SHIFT,   FN,      KC_RALT,          KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LGUI, KC_LALT, SYMBOL,                    NAV_SPC,                            SHIFT,   FN,      KC_RALT,          KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 [_COLEMAK_FN] = LAYOUT_ansi_82(
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGDN,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_HOME,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT, KC_UP,
-    KC_LCTL, KC_LCMD, KC_LALT,                   KC_SPC,                             KC_RALT, FN_Q,    KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, FN_Q,    KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 [_QWERTY_FN] = LAYOUT_ansi_82(
